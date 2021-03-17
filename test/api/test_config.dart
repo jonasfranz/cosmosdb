ab1b0f20faf1042ab1c5cf957a22464f7f00388b
Jonas Franz <jonas.franz@incloud.de>
2021-03-17 23:29:24 +0100

    Add emulator credentials

--- test/api/test_config.dart
+++ test/api/test_config.dart
@@ -1,8 +1,10 @@
 class TestConfig {
   const TestConfig._();
 
-  static const cosmosDBUrl =
-      String.fromEnvironment('COSMOS_DB_URL', defaultValue: '');
-  static const cosmosDBMasterKey =
-      String.fromEnvironment('COSMOS_DB_MASTER_KEY', defaultValue: '');
+  static const cosmosDBUrl = String.fromEnvironment('COSMOS_DB_URL',
+      defaultValue: 'https://localhost:8081');
+  static const cosmosDBMasterKey = String.fromEnvironment(
+      'COSMOS_DB_MASTER_KEY',
+      defaultValue:
+          'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==');
 }

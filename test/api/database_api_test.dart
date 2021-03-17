a16a62bb2f184c16116fd7db36bf9dbd139f33f7
Jonas Franz <jonas.franz@incloud.de>
2021-03-18 00:58:22 +0100

    Complete database api test

--- test/api/database_api_test.dart
+++ test/api/database_api_test.dart
@@ -7,13 +7,30 @@ void main() {
   group('database api test', () {
     final cosmos = buildClient();
 
-    test('check if creating a database works', () async {
+    test('check if creating and deleting a database works', () async {
       final databaseId = generateDatabaseName();
       expect(
           await cosmos.databases
               .create(Database(id: databaseId))
               .then((db) => db.id),
           databaseId);
+      await cosmos.databases.delete(databaseId);
+    });
+
+    test('check if listing databases works', () async {
+      final databaseId = generateDatabaseName();
+      await cosmos.databases.create(Database(id: databaseId));
+      final dbs = await cosmos.databases.list();
+      expect(dbs.toList()[0].id, databaseId);
+      await cosmos.databases.delete(databaseId);
+    });
+
+    test('check if findById works', () async {
+      final databaseId = generateDatabaseName();
+      await cosmos.databases.create(Database(id: databaseId));
+      final db = await cosmos.databases.findById(databaseId);
+      expect(db.id, databaseId);
+      await cosmos.databases.delete(databaseId);
     });
   });
 }
